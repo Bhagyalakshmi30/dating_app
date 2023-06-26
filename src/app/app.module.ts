2f85a98a66e3e4ca6ecd37c2c900ab10cccb0837
Bhagyalakshmi30 <bhagyalashmi.kanini@gmail.com>
2023-06-26 20:03:09 +0530

    adding interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,8 @@ import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { ServerErrorComponent } from './errors/server-error/server-error.component';
 import { MembersCardComponent } from './members/members-card/members-card.component';
 import { JwtInterceptor } from './_interceptor/jwt.interceptor';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
+import { LoadingInterceptor } from './_interceptor/loading.interceptor';
 
 @NgModule({
   declarations: [
@@ -32,7 +34,8 @@ import { JwtInterceptor } from './_interceptor/jwt.interceptor';
     TestErrorComponent,
     NotFoundComponent,
     ServerErrorComponent,
-    MembersCardComponent
+    MembersCardComponent,
+    MemberEditComponent
   ],
   imports: [
     BrowserModule,
@@ -45,7 +48,9 @@ import { JwtInterceptor } from './_interceptor/jwt.interceptor';
   ],
   providers: [
     {provide: HTTP_INTERCEPTORS, useClass:ErrorInterceptor, multi: true},
-    {provide: HTTP_INTERCEPTORS, useClass:JwtInterceptor, multi: true}
+    {provide: HTTP_INTERCEPTORS, useClass:JwtInterceptor, multi: true},
+    {provide: HTTP_INTERCEPTORS, useClass:LoadingInterceptor, multi: true},
+
 
   ],
   bootstrap: [AppComponent]
